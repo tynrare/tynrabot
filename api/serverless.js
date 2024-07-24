9b25b79f2597e45400454b5aead4cd41ca0970f8
tynroar <tynrare@gmail.com>
2024-07-24 20:50:22 +0300

    server score api done maybe

--- api/serverless.js
+++ api/serverless.js
@@ -5,12 +5,13 @@ const app = Fastify({ logger: true })
 app.register(require('../src/bot'))
 app.register(require('../src/webhook'))
 app.get('/',  (req, rep) => rep.send(
-  'Hi! This is a Telegram bot server 🤖\n' +
-  '📣 Built with Telegraf\n' +
-  '⚡ Spun up using Fastify\n' +
-  '🚀 Hosted on Vercel\n' +
-  'The webhook is hidden in a secret path that only Telegram knows 🦾'
+  'https://t.me/tynrabot\n' +
+  'https://github.com/tynrare/tynrabot\n'
 ))
+
+app.get("/ping", (req, res) => {
+  res.json({ pong: true });
+});
 
 module.exports = async (req, res) => {
   await app.ready();
--- src/bot.js
+++ src/bot.js
@@ -1,55 +1,109 @@
-const fastifyPlugin = require('fastify-plugin')
-const { Telegraf } = require('telegraf')
+const fastifyPlugin = require("fastify-plugin");
+const { Telegraf } = require("telegraf");
 const { message } = require("telegraf/filters");
 const jws = require("jws");
 
+function init_bot(app) {
+  const bot = new Telegraf(process.env.BOT_TOKEN);
 
-module.exports = fastifyPlugin(async (app) => {
-    const bot = new Telegraf(process.env.BOT_TOKEN)
-
-    const games = {
+  const games = {
     booling: (token) =>
-        `https://witgs-threejs.netlify.app/?mode=prod&token=${token}#splashscreen_bowling`,
-    };
+      `https://witgs-threejs.netlify.app/?mode=prod&token=${token}#splashscreen_bowling`,
+  };
 
-    bot.start((ctx) => ctx.reply("Welcome"));
-    bot.help((ctx) => ctx.reply("Send me a sticker"));
-    bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
-    bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+  bot.start((ctx) => ctx.reply("Welcome"));
+  bot.help((ctx) => ctx.reply("Send me a sticker"));
+  bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
+  bot.hears("hi", (ctx) => ctx.reply("Hey there"));
 
-    const inlineAnswer = [];
-    for (const k in games) {
+  const inlineAnswer = [];
+  for (const k in games) {
     inlineAnswer.push({
-        type: "game",
-        id: k,
-        game_short_name: k,
+      type: "game",
+      id: k,
+      game_short_name: k,
     });
-    }
+  }
 
-    bot.on("inline_query", async (ctx) => {
+  bot.on("inline_query", async (ctx) => {
     const query = ctx.inlineQuery.query;
     return await ctx.answerInlineQuery(inlineAnswer);
-    });
+  });
 
-    bot.gameQuery((ctx) => {
+  bot.gameQuery((ctx) => {
     const game = games[ctx.callbackQuery.game_short_name];
     const token = jws.sign({
-        header: { alg: "HS512" },
-        payload: {
+      header: { alg: "HS512" },
+      payload: {
         game: game,
         user: ctx.from.id,
         imessage: ctx.callbackQuery.inline_message_id,
         message: (ctx.callbackQuery.message || {}).message_id,
         chat: (ctx.chat || {}).id,
-        },
-        secret: process.env.SIGN_SECRET,
+      },
+      secret: process.env.SIGN_SECRET,
     });
     ctx.answerGameQuery(games[game](token));
-    });
+  });
 
-    // Enable graceful stop
-    process.once('SIGINT', () => bot.stop('SIGINT'))
-    process.once('SIGTERM', () => bot.stop('SIGTERM'))
+  // Enable graceful stop
+  process.once("SIGINT", () => bot.stop("SIGINT"));
+  process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
-    app.decorate('bot', bot)
-})
+  app.decorate("bot", bot);
+
+  return bot;
+}
+
+function init_score(app, bot) {
+  const check_token = (req, reply) => {
+    const token = url.parse(req.headers.referer).query.slice(6);
+    if (!jws.verify(token, "HS512", process.env.SIGN_SECRET)) {
+      return reply.code(403).send("wrong token");
+    }
+    req.game_body = JSON.parse(jws.decode(token).payload);
+
+		return null;
+  };
+
+	app.get("/score", (request, reply) => {
+		if (check_token(req, reply)) {
+			return;
+		}
+
+    const { user, imessage, chat, message } = req.game_body;
+    bot.telegram
+      .getGameHighScores(user, imessage, chat, message)
+      .then((scores) => {
+				res.send(scores);
+      })
+      .catch((err) => {
+        console.log(err);
+				res.code(500).send(err);
+      });
+	});
+	app.post("/score", (request, reply) => {
+		if (check_token(req, reply)) {
+			return;
+		}
+
+    const scoreValue = parseInt(request.body.score);
+    if (scoreValue <= 0) {
+      return reply.code(400).send("score can't be negatice");
+    }
+    const { user, imessage, chat, message } = request.game_body;
+    telegram
+      .setGameScore(user, scoreValue, imessage, chat, message, true)
+      .then(() => {
+				reply.send();
+      })
+      .catch((err) => {
+				reply.code(err.code || 500).send(err);
+      });
+	});
+}
+
+module.exports = fastifyPlugin(async (app) => {
+  const bot = init_bot(app);
+  init_score(app, bot);
+});
