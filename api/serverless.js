6f90a46214b6789df82cb522cc87ecb87c103d9c
tynroar <tynrare@gmail.com>
2024-07-24 22:40:46 +0300

    test cors

--- api/serverless.js
+++ api/serverless.js
@@ -13,6 +13,24 @@ app.get("/ping", (request, reply) => {
 	reply.send("pong");
 });
 
+app.addHook('preHandler', (req, res, done) => {
+
+  // example logic for conditionally adding headers
+  const allowedPaths = [ "/score" ];
+  if (allowedPaths.includes(req.routerPath)) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST,GET");
+    res.header("Access-Control-Allow-Headers",  "*");
+  }
+
+  const isPreflight = /options/i.test(req.method);
+  if (isPreflight) {
+    return res.send();
+  }
+
+  done();
+})
+
 module.exports = async (req, res) => {
   await app.ready();
   app.server.emit('request', req, res);
